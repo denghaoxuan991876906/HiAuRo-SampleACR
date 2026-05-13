1ff9c356fcfa919df26b8d0d2e9cc412ad556989
嗨呀www <991876906@qq.com>
2026-05-13 22:36:33 +0800

    Init

---
+++ SampleJob/SampleEntry.cs
@@ -0,0 +1,40 @@
+﻿using HiAuRo.ACR;
+
+namespace SampleACR.SampleJob;
+
+public class SampleEntry : IRotationEntry
+{
+    public Rotation? Build(string settingFolder)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IRotationUI? GetRotationUI()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDrawSetting()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnEnterRotation()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnExitRotation()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string AuthorName { get; } = "Sample";
+    public bool UseCustomUi { get; } = false;
+    public IEnumerable<Jobs> TargetJobs { get; } = [Jobs.None];
+}
