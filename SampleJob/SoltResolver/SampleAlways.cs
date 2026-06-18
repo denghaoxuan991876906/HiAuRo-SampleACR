d39825a7874814c77534690ce4551715d7a33f9c
嗨呀www <991876906@qq.com>
2026-06-18 13:48:19 +0800

    更新

--- SampleJob/SoltResolver/SampleAlways.cs
+++ SampleJob/SoltResolver/SampleAlways.cs
@@ -6,7 +6,7 @@ public class SampleAlways:ISlotResolver
 {
     public int Check()
     {
-        _ = HelperRuntime.IsInCombat();
+        _ = GameHelper.IsInCombat();
         if (Data.Target.Current.DistanceToPlayer() < 10)
             return 1;
         return -1;
